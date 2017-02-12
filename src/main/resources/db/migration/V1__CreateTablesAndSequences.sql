c30652a250a03c007aa33f472a9813abc3183441
Marcelo Ortiz De Santana <tentativafc@hotmail.com>
2017-02-12 11:06:39 -0200

    fix migration file.

--- src/main/resources/db/migration/V1__CreateTablesAndSequences.sql
+++ src/main/resources/db/migration/V1__CreateTablesAndSequences.sql
@@ -2,8 +2,8 @@
 CREATE TABLE "user"
 (
     id BIGINT NOT NULL,
-    login VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
